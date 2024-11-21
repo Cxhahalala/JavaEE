6ba1c25b4af2dd2db50efc8573d32becc7a67c24
Cxhahalala <163889059+Cxhahalala@users.noreply.github.com>
2024-11-21 13:42:32 +0800

    Delete src directory

--- src/main/java/com/example/demochapter05/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demochapter05.dao;
-
-
-import com.example.demochapter05.entity.User;
-import  org.springframework.data.repository.CrudRepository;
-import  org.springframework.stereotype.Repository;
-@Repository("jpaUserRepository")
-public  interface  UserRepository extends  CrudRepository<User,Integer> {
-}
